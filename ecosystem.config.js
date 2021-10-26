b29df5b926f20eeeed0194bb229c714505eefd85
Reazul Iqbal <admin@reazul.net>
2021-10-26 16:02:19 +0600

    Deployer update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,18 @@ module.exports = {
     args: 'start',
     env_production: {
       NODE_ENV: 'production',
-      API_BASE_URL: 'https://defidojo.dtools.dev/api'
+      API_BASE_URL: 'https://defidojo.dev/api'
     }
-  }]
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: ['defidojo.dev'],
+      ref: 'origin/master',
+      repo: 'git@github.com:hive-engine/defidojo-frontend.git',
+      ssh_options: ['ForwardAgent=yes'],
+      path: '/home/ubuntu/nodejs/frontend',
+      'post-deploy': 'npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production --update-env && pm2 save'
+    }
+  }
 }
